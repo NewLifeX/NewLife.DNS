0f25e8f65831567e08e4c0ad06a13c2d94506ffc
大石头 <nnhy@vip.qq.com>
2016-12-11 17:09:36 +0800

    更换图标，去掉Web里面用不到的引用

--- NewLife.DNS.Web/Global.asax.cs
+++ NewLife.DNS.Web/Global.asax.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace NewLife.DNS.Web
